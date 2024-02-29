37cc996bab354a23e48755712daecf125e301c06
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-02-29 16:52:59 -0300

    Criando arquivo para as funções comuns entre as classes de interface gráfica

---
+++ app/src/main/kotlin/io/github/henriquemcc/computacaografica/trabalhopratico/visao/FuncoesGraficas.kt
@@ -0,0 +1,8 @@
+package io.github.henriquemcc.computacaografica.trabalhopratico.visao
+
+import java.awt.Dimension
+import java.awt.Toolkit
+
+fun obterResolucaoTela(): Dimension {
+    return Toolkit.getDefaultToolkit().screenSize
+}
