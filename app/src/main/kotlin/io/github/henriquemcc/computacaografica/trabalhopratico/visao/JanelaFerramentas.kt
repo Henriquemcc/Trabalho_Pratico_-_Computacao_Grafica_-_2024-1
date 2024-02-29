f54e1239cf8c64d91294314a37cb7bfc0bd02594
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-02-29 16:59:05 -0300

    Alterando o tamanho da janela de ferramentas

--- app/src/main/kotlin/io/github/henriquemcc/computacaografica/trabalhopratico/visao/JanelaFerramentas.kt
+++ app/src/main/kotlin/io/github/henriquemcc/computacaografica/trabalhopratico/visao/JanelaFerramentas.kt
@@ -1,14 +1,15 @@
 package io.github.henriquemcc.computacaografica.trabalhopratico.visao
 
 import javax.swing.JFrame
+import kotlin.math.ceil
 
 class JanelaFerramentas {
     val jFrame = JFrame("Ferramentas")
 
     init {
         jFrame.defaultCloseOperation = JFrame.DO_NOTHING_ON_CLOSE
-        jFrame.size = obterResolucaoTela()
-        jFrame.extendedState = JFrame.MAXIMIZED_BOTH
+        jFrame.size.width = 20
+        jFrame.size.height = ceil(obterResolucaoTela().height * 0.8).toInt()
         jFrame.isVisible = true
     }
 }
