2e6c51302febe53228fadc698af597f9879da062
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-02-29 16:45:57 -0300

    Adicionando a construção da interface gráfica

--- app/src/main/kotlin/io/github/henriquemcc/computacaografica/trabalhopratico/App.kt
+++ app/src/main/kotlin/io/github/henriquemcc/computacaografica/trabalhopratico/App.kt
@@ -3,6 +3,8 @@
  */
 package io.github.henriquemcc.computacaografica.trabalhopratico
 
+import io.github.henriquemcc.computacaografica.trabalhopratico.visao.InterfaceGrafica
+
 class App {
     val greeting: String
         get() {
@@ -12,4 +14,6 @@ class App {
 
 fun main() {
     println(App().greeting)
+    val interfaceGrafica = InterfaceGrafica()
+
 }
