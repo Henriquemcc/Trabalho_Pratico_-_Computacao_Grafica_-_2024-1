384cd646955f12b40a47798d65689176cfe3d220
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-03-04 20:32:49 -0300

    Corrigindo erro de definição da classe ponto

--- app/src/main/kotlin/io/github/henriquemcc/computacaografica/trabalhopratico/modelo/Ponto.kt
+++ app/src/main/kotlin/io/github/henriquemcc/computacaografica/trabalhopratico/modelo/Ponto.kt
@@ -1,5 +1,5 @@
 package io.github.henriquemcc.computacaografica.trabalhopratico.modelo
 
-class Ponto(x: Int, y: Int): ElementoGrafico() {
+class Ponto(val x: Int, val y: Int): ElementoGrafico() {
 
 }
