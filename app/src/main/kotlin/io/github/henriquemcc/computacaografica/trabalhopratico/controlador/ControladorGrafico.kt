9aa56dd7b4912e5de8a15812c2909cdbc93a8103
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-03-06 18:13:21 -0300

    Implementando funções para obter reta e circunferência

--- app/src/main/kotlin/io/github/henriquemcc/computacaografica/trabalhopratico/controlador/ControladorGrafico.kt
+++ app/src/main/kotlin/io/github/henriquemcc/computacaografica/trabalhopratico/controlador/ControladorGrafico.kt
@@ -1,18 +1,48 @@
 package io.github.henriquemcc.computacaografica.trabalhopratico.controlador
 
+import io.github.henriquemcc.computacaografica.trabalhopratico.modelo.AlgoritmoReta
+import io.github.henriquemcc.computacaografica.trabalhopratico.modelo.Ponto
+import io.github.henriquemcc.computacaografica.trabalhopratico.visao.obterAlgoritmoReta
 import java.awt.event.MouseEvent
+import javax.swing.ButtonGroup
+import javax.swing.JFrame
+import javax.swing.JRadioButton
 
 class ControladorGrafico
 {
+	/* Classes para a realização de operações internas */
+	private abstract class OperacaoGrafica(){}
+
+
+	private class OperacaoReta(): OperacaoGrafica() {
+		var pontoInicial: Ponto? = null
+		var pontoFinal: Ponto? = null
+
+
+
+		var algoritmoReta: AlgoritmoReta? = null
+	}
+
+	private class OperacaoCircunferencia(): OperacaoGrafica() {
+		var centro: Ponto? = null
+		var raio: Int = 0
+	}
+
+	companion object
+	{
+		private var operacaoGrafica: OperacaoGrafica? = null
+	}
+
 	/* Funções executadas pelo ButtonHandler: */
 	fun ativarObtencaoReta()
 	{
-		TODO("Not yet implemented")
+		operacaoGrafica = OperacaoReta()
+		(operacaoGrafica as OperacaoReta).algoritmoReta = obterAlgoritmoReta()
 	}
 
 	fun ativarObtencaoCircunferencia()
 	{
-		TODO("Not yet implemented")
+		operacaoGrafica = OperacaoCircunferencia()
 	}
 
 	fun ativarObtencaoTranslacao()
@@ -49,7 +79,30 @@ class ControladorGrafico
 
 	fun clique(event: MouseEvent)
 	{
-		TODO("Not yet implemented")
+		when (operacaoGrafica)
+		{
+			is OperacaoReta -> tratarCliqueOperacaoReta(event)
+			is OperacaoCircunferencia -> tratarCliqueOperacaoCircunferencia(event)
+
+		}
+	}
+
+	private fun tratarCliqueOperacaoCircunferencia(event: MouseEvent)
+	{
+
+	}
+
+	private fun tratarCliqueOperacaoReta(event: MouseEvent)
+	{
+		if ((operacaoGrafica as OperacaoReta).pontoInicial == null)
+		{
+			(operacaoGrafica as OperacaoReta).pontoInicial = Ponto(event.x, event.y)
+		}
+		else if ((operacaoGrafica as OperacaoReta).pontoFinal == null)
+		{
+			(operacaoGrafica as OperacaoReta).pontoFinal = Ponto(event.x, event.y)
+
+		}
 	}
 
 }
